c4d320d36515d96960d48bcf482e5d9ec1510e3a
jotmomi04 <w07779181@myscc>
2022-02-25 01:53:52 -0500

    Update Controler

---
+++ Controllers/BowlMaker.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SinghBowlShop.Controllers
+{
+    public class BowlMaker : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
